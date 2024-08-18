06caca8cb52f3206f2ed75b6384c3988e43d77f1
LeThiPhuongAnh178 <phuonganhlethi43@gmail.com>
2024-08-18 16:56:18 +0700

    Add files via upload

---
+++ vd1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 13:18:39 2024
+
+@author: Lê Thị Phương Anh
+"""
+
+distance = float(input("Nhập độ dài đường đến trường(m): "))
+
+if distance > 1200:
+    print("Đường đến trường quá xa.")
